64b46c061f3fb2c26f1dbfb860e6c737552ef263
Juan Asencio <kontakt@asencio.dk>
2013-07-09 18:02:09 +0000

    Update web.js returned to hello2

--- web.js
+++ web.js
@@ -4,10 +4,6 @@ var app = express.createServer(express.app());
 
 logger.get('/', function(request, response) {
   response.send('Hello World 2!');
-   // var fs = require('fs');
-   // var buffer = new Buffer(16);
-   // var message = fs.readFileSync(index.html);
-   // buffer.write(message);
 });
 
 var port = process.env.PORT || 5000;
