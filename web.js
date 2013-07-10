d474dd033c15a6cbd6f2a1fb3418d7674de20574
Juan Asencio <kontakt@asencio.dk>
2013-07-10 09:03:55 +0000

    Update web.js fs.read function with buffer.toString and buffer.write, added buffer size

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
     var fs = require('fs');
-    var buffer = new Buffer();
+    var buffer = new Buffer(64);
     response.send(buffer.toStrint('uft8', fs.readFileSync('index.html')));
 
 //	var buffer = new Buffer(16);
